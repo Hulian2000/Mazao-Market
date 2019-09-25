f9e1231bae244b3131328c19a6b69e58810b7fc0
moharick <moharick@gmail.com>
2019-09-25 15:03:15 +0300

    Add all code for the landing page.

---
+++ app/auth/views.py
@@ -0,0 +1,16 @@
+from app.auth import auth
+
+
+@auth.route('/login')
+def login():
+    pass
+
+
+@auth.route('/signup')
+def signup():
+    pass
+
+
+@auth.route('/logout')
+def logout():
+    pass
