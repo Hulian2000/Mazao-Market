77492fdfacd3df5da2a9cc7cd90f11dda17e518b
Ndundiro_Kamau <ndundirokamau@gmail.com>
2019-09-25 16:48:56 +0300

    Adds the custom validation to ensure that username and email entered by new users does not exists on the database.

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm 
 from wtforms import StringField,PasswordField,SubmitField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from app.models import User
 
 
 class SignupForm(FlaskForm):
@@ -10,4 +11,22 @@ class SignupForm(FlaskForm):
     password=PasswordField("password",validators=[DataRequired()])
     confirm_password=PasswordField("Confirm Password",validators=[DataRequired(),EqualTo("password")])
     submit=SubmitField("submit") 
+
+
+    '''
+    Custom Validation to ensure that a user with that username does not exist in our database.
+    '''
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('A user with that username already exists.Please choose another user')
+    
+    '''
+    Custom Validation to ensure that a user with that email does not exist in our database.
+    '''
+
+    def validate_email(swlf, email):
+        email = User.query.filter_by(email=email.data).first()
+        if email:
+            raise ValidationError('That email is already taken,please use another email.')
 
