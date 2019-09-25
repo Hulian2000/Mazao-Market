bbfa0725b8410259c022888dc048a69a11a3c989
Hulian2000 <prepmoringa11@gmail.com>
2019-09-25 11:38:16 +0300

    add forms on log in and log out

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,50 +1,22 @@
-import pymysql
-from app import app
-from db_config import mysql
-from flask import flash, session, render_template, request, redirect
-from werkzeug import generate_password_hash, check_password_hash
-
-@app.route('/')
-def index():
-	if 'email' in session:
-		username = session['email']
-		return 'Logged in as ' + username + '<br>' + "<b><a href = '/logout'>click here to logout</a></b>"
-	return "You are not logged in <br><a href = '/login'></b>" + "click here to login</b></a>"
-	
-@app.route('/login')
-def login():
-	return render_template('login.html')
-
-@app.route('/submit', methods=['POST'])
-def login_submit():
-	_email = request.form['inputEmail']
-	_password = request.form['inputPassword']
-	# validate the received values
-	if _email and _password and request.method == 'POST':
-		#check user exists			
-		conn = mysql.connect()
-		cursor = conn.cursor()
-		sql = "SELECT * FROM tbl_user WHERE user_email=%s"
-		sql_where = (_email,)
-		cursor.execute(sql, sql_where)
-		row = cursor.fetchone()
-		if row:
-			if check_password_hash(row[3], _password):
-				session['email'] = row[1]
-				cursor.close() 
-				conn.close()
-				return redirect('/')
-			else:
-				flash('Invalid password!')
-				return redirect('/login')
-		else:
-			flash('Invalid email/password!')
-			return redirect('/login')
-		
-@app.route('/logout')
-def logout():
-	session.pop('email', None)
-	return redirect('/')
-
-if __name__ == "__main__":
-    app.run()
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField,BooleanField
+from wtforms.validators import Required, Email, EqualTo
+from ..models import User
+from wtforms import ValidationError
+class LoginForm(FlaskForm):
+   email = StringField('Your Email Address', validators=[Required(), Email()])
+   password = PasswordField('Password', validators=[Required()])
+   remember = BooleanField('Remember me')
+   submit = SubmitField('Sign In')
+class RegistrationForm(FlaskForm):
+   email = StringField('Your Email Address', validators=[Required(), Email()])
+   username = StringField('Enter your username',validators=[Required()])
+   password = PasswordField('Password', validators=[Required(), EqualTo('password_confirm', message = 'Passwords must match')])
+   password_confirm = PasswordField('Confirm Passwords', validators=[Required()])
+   submit = SubmitField('Sign Up')
+   def validate_email(self, data_field):
+       if User.query.filter_by(email = data_field.data).first():
+           raise ValidationError('There is an account with that email')
+   def validate_username(self, data_field):
+       if User.query.filter_by(username = data_field.data).first():
+           raise ValidationError('That username is taken')
