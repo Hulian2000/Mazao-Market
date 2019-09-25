7ecdacedc3e884456219b75a3e09ee4152979f16
Ndundiro_Kamau <ndundirokamau@gmail.com>
2019-09-25 16:01:05 +0300

    Adds the SignupForm class that defines the signup form inputs.

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,1 +1,13 @@
 from flask_wtf import FlaskForm 
+from wtforms import StringField,PasswordField,SubmitField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class SignupForm(FlaskForm):
+    username=StringField("username", validators=[DataRequired()])
+    email=StringField("email",validators=[DataRequired(),Email()])
+    location=StringField("location",validators=[DataRequired()])
+    password=PasswordField("password",validators=[DataRequired()])
+    confirm_password=PasswordField("Confirm Password",validators=[DataRequired(),EqualTo("password")])
+    submit=SubmitField("submit") 
+
