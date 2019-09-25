a36559b261064c6db7647fd987ee5c8a9a186d6a
Hulian2000 <prepmoringa11@gmail.com>
2019-09-25 14:50:20 +0300

    add files to view.py

--- app/main/views.py
+++ app/main/views.py
@@ -27,12 +27,12 @@ def logout():
 def register():
    form = RegistrationForm()
    if form.validate_on_submit():
-       user = User(email = form.email.data, username = form.username.data, password = form.password.data)
-       db.session.add(user)
-       db.session.commit()
-       mail_message("Welcome to Watchtower", "email/welcome_user", user.email, user = user)
-       return redirect(url_for('auth.login'))
-       title = "New Account"
+        user = User(email = form.email.data, username = form.username.data, password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        mail_message("Welcome to Watchtower", "email/welcome_user", user.email, user = user)
+        return redirect(url_for('auth.login'))
+        title = "New Account"
    return render_template('auth/register.html', registration_form = form)
 
 
