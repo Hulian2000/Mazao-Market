8489c18e4c0770822016e7b31eeaf1252122275a
kamauvick <kamauvick@outlook.com>
2019-09-25 09:46:34 +0300

    Space out the methods in the models.py file.

--- app/models.py
+++ app/models.py
@@ -1,16 +1,19 @@
-from app import db
 from datetime import datetime
-from werkzeug.security import generate_password_hash,check_password_hash
+
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from app import db
 
 
 class User(db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(120), unique = True, nullable = False)
-    email = db.Column(db.String(255), unique = True, nullable = False)
-    location = db.Column(db.String(120), nullable = False)
-    datejoined = db.Column(db.DateTime,default=datetime.utcnow)
-    hash_password = db.Column(db.String(255),nullable = False)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    location = db.Column(db.String(120), nullable=False)
+    datejoined = db.Column(db.DateTime, default=datetime.utcnow)
+    hash_password = db.Column(db.String(255), nullable=False)
+
     @property
     def set_password(self):
         raise AttributeError('You cannot read the password attribute')
@@ -19,16 +22,16 @@ class User(db.Model):
     def password(self, password):
         self.hashed_password = generate_password_hash(password)
 
-
-    def verify_password(self,password):
-        return check_password_hash(self.hashed_password,password)
+    def verify_password(self, password):
+        return check_password_hash(self.hashed_password, password)
 
     def save(self):
         db.session.add(self)
         db.session.commit()
+
     def delete(self):
         db.session.delete(self)
         db.session.commit()
 
     def __repr__(self):
-        return "User: %s" %str(self.username)
+        return "User: %s" % str(self.username)
