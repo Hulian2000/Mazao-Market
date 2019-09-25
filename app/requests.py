27a40f5caff41918b1981930d9e82a6222185f9f
kamauvick <kamauvick@outlook.com>
2019-09-25 09:44:58 +0300

    Add the template for making requests.

---
+++ app/requests.py
@@ -0,0 +1,34 @@
+# import requests
+
+"""
+
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    This file contains the calls to the weather and news API's and how to consume them using requests 
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+"""
+
+""" 
+     SAMPLE WEATHER API REQUEST
+     request = 'http://api.openweathermap.org/data/2.5/weather?q=Kiambu&appid=6d98967004f5e634642db86f5f402d9e'     
+"""
+
+""" 
+     SAMPLE NEWS API REQUEST
+     request = ''     
+"""
+
+
+# Get weather data function
+def getWeatherData():
+    pass
+
+
+# Get agricultural news function
+def getAgriNews():
+    pass
+
+
+# Get Trending news function
+def getTrendingNews():
+    pass
