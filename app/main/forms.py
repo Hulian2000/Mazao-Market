09b1608dc8c89888ea6be96d1e9bcf5142c7f17f
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-09-25 15:41:21 +0300

    Create Blog view to

---
+++ app/main/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import Required
+
+
+class BlogForm(FlaskForm):
+    title = StringField('Blog Title', validators=[Required()])
+    post = TextAreaField('Post Content', validators=[Required()])
+    submit = SubmitField('Post')
--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,26 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 
+from app.models import Blog
 from app.main import main
+
+from .forms import BlogForm
+
+from flask_login import login_required, current_user
 
 
 @main.route('/')
 def index():
     return render_template('index.html')
+
+
+@main.route('/blog')
+@login_required
+def create_blog():
+    form = BlogForm()
+    if form.validate_on_submit():
+        new_blog = Blog(title=form.title.data, post=form.post.data, user_id=current_user.get_current_object().id)
+        new_blog.save_blog()
+        return redirect(url_for('main.index'))
+    return render_template('new_blog.html', form=form)
+
+
