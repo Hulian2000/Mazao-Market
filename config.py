4146eaa549d223cd3cb1f264c6264b6b077bc0a1
kamauvick <kamauvick@outlook.com>
2019-09-25 15:36:23 +0300

    Add code for the requests

--- config.py
+++ config.py
@@ -1,7 +1,14 @@
 import os
+
 from dotenv import load_dotenv
+
 load_dotenv()
+
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     DEBUG = True
+    RAPID_API_HOST = os.environ.get('RAPID_API_HOST')
+    RAPID_API_KEY = os.environ.get('RAPID_API_KEY')
+    RAPID_API_URL = os.environ.get('RAPID_API_URL')
