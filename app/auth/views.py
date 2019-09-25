d1f6d73125eab649e4bcecc4f8dff33d2263844d
Ndundiro_Kamau <ndundirokamau@gmail.com>
2019-09-25 15:57:24 +0300

    Adds the signup route function.Successfully creates a user and adds them to the database.

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,4 +1,10 @@
 from app.auth import auth
+from .. import app,db,bcrypt
+from app.models import User
+from flask import render_template, url_for, flash, redirect
+from .forms import SignupForm
+
+
 
 
 @auth.route('/login')
@@ -6,9 +12,18 @@ def login():
     pass
 
 
-@auth.route('/signup')
+@auth.route('/signup', methods = ['GET','POST'])
 def signup():
-    pass
+    form=SignupForm()
+    if form.validate_on_submit():
+        hashed_password=bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, location=form.location.data, hash_password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash(f"Account has been created!","success")
+        return redirect(url_for('main.index'))
+    return render_template('auth/signup.html',title="SignUp", form=form )
+    
 
 
 @auth.route('/logout')
