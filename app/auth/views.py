831acba1873a3ed41394a202a3e3a2f6b757fabd
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-09-26 16:07:21 +0300

    Update login and signup

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,10 +1,10 @@
 from flask import render_template, flash, request, redirect, url_for
 from flask_login import login_user, logout_user, login_required
 from app.auth import auth
-from .. import app,db
+from .. import app, db
 from app.models import User
 from flask import render_template, url_for, flash, redirect
-from .forms import SignupForm
+from .forms import SignupForm, LoginForm
 
 
 @auth.route('/login', methods=['POST', 'GET'])
@@ -12,25 +12,24 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
-        if user != None and user.verify_password(form.password.data):
+        if user is not None and user.verify_password(form.password.data):
             login_user(user, form.remember.data)
             return redirect(request.args.get('next') or url_for('main.index'))
         flash('Wrong Username or Password')
     return render_template('auth/login.html', form=form)
 
 
-
-@auth.route('/signup', methods = ['GET','POST'])
+@auth.route('/signup', methods=['GET', 'POST'])
 def signup():
-    form=SignupForm()
+    form = SignupForm()
     if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data, location=form.location.data, password=form.password.data)
+        user = User(username=form.username.data, email=form.email.data, location=form.location.data,
+                    password=form.password.data)
         db.session.add(user)
         db.session.commit()
-        flash(f"Account has been created!","success")
-        return redirect(url_for('main.index'))
-    return render_template('auth/signup.html',title="SignUp", form=form )
-
+        flash(f"Account has been created!", "success")
+        return redirect(url_for('main.login'))
+    return render_template('auth/signup.html', title="SignUp", form=form)
 
 
 @auth.route('/logout')
