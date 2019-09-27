a2ad00966eddbf859ebcaac91bc0093febc8e65f
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-09-27 07:41:57 +0300

    update login

--- app/models.py
+++ app/models.py
@@ -1,4 +1,3 @@
-
 from datetime import datetime
 
 from flask_login import current_user, UserMixin
@@ -12,19 +11,20 @@ from app import db, login_manager
 def load_user(user_id):
     return User.query.get(user_id)
 
+
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120), unique=True, nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
     location = db.Column(db.String(120), nullable=False)
+    image_path = db.Column(db.String(255), default='')
     datejoined = db.Column(db.DateTime, default=datetime.utcnow)
     hash_password = db.Column(db.String(255), nullable=False)
     post = db.relationship('Blog', backref='user', lazy='dynamic')
     like = db.relationship('Like', backref='user', lazy='dynamic')
     dislike = db.relationship('Dislike', backref='user', lazy='dynamic')
     comment = db.relationship('Comment', backref='user', lazy=True)
-
 
     @property
     def password(self):
@@ -34,10 +34,8 @@ class User(db.Model, UserMixin):
     def password(self, password):
         self.hash_password = generate_password_hash(password)
 
-
     def verify_password(self, password):
         return check_password_hash(self.hash_password, password)
-
 
     def save(self):
         db.session.add(self)
@@ -62,7 +60,6 @@ class Blog(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     comment = db.relationship('Comment', backref='blog', lazy='dynamic')
 
-
     def save_blog(self):
         db.session.add(self)
         db.session.commit()
@@ -70,7 +67,6 @@ class Blog(db.Model):
     def delete_blog(self):
         db.session.delete(self)
         db.session.commit()
-
 
     def get_blog(id):
         blog = Blog.query.filter_by(id=id).first()
@@ -89,7 +85,6 @@ class Comment(db.Model):
     blog_id = db.Column(db.Integer, db.ForeignKey("blogs.id"))
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
 
-
     def save(self):
         db.session.add(self)
         db.session.commit()
@@ -101,7 +96,6 @@ class Comment(db.Model):
     def get_comment(id):
         comment = Comment.query.all(id=id)
         return comment
-
 
     def __repr__(self):
         return f'Comment {self.comment}'
@@ -132,17 +126,14 @@ class Dislike(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     blog_id = db.Column(db.Integer, db.ForeignKey('blogs.id'))
 
-
     def save(self):
         db.session.add(self)
         db.session.commit()
 
     @classmethod
-
     def get_dislikes(cls, id):
         dislikes = Dislike.query.filter_by(blog_id=id).all()
         return dislikes
 
     def __repr__(self):
         return f'{self.user_id}:{self.blog_id}'
-
