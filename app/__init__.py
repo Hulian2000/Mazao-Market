0bc56eea5fb34bdfdbbd5abe42b02184fc13a287
Ndundiro_Kamau <ndundirokamau@gmail.com>
2019-09-27 09:11:52 +0300

    Adds the configurations that enable sending of reset password.

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from config import Config
+##################################
+from flask_mail import Mail
 
 app = Flask(__name__)
 login_manager = LoginManager(app)
@@ -10,6 +13,15 @@ login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
+################################
+app.config['MAIL_SERVER']= 'smtp.gmail.com'  
+app.config['MAIL_PORT']= 587
+app.config['MAIL_USE_TLS']= True
+
+app.config['MAIL_USERNAME']= os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD']= os.environ.get('EMAIL_PASS')
+
+mail = Mail(app)
 
 def create_app():
     app.config.from_object(Config)
--- config.py
+++ config.py
@@ -6,9 +6,10 @@ load_dotenv()
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY','')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     DEBUG = True
     RAPID_API_HOST = os.environ.get('RAPID_API_HOST')
     RAPID_API_KEY = os.environ.get('RAPID_API_KEY')
     RAPID_API_URL = os.environ.get('RAPID_API_URL')
+
