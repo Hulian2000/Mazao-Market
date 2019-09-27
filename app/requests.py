5388301814d96210f2fa3f15310e2296ce59934f
kamauvick <kamauvick@outlook.com>
2019-09-27 08:48:35 +0300

    Remove debug print statements in the requests.py file.
    Update the news and base html templates.

--- app/requests.py
+++ app/requests.py
@@ -1,7 +1,7 @@
 import requests
-from flask_login import current_user
-from config import Config
+
 from app.models import User
+from config import Config
 
 """
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -12,7 +12,9 @@ from app.models import User
 user = User.query.all()
 for i in user:
     location = i.location
-    print(location)
+    # print(location)
+
+
 # SAMPLE WEATHER API REQUEST
 # request = 'http://api.openweathermap.org/data/2.5/weather?q=Kiambu&appid=6d98967004f5e634642db86f5f402d9e'
 
@@ -23,26 +25,10 @@ def getWeatherData():
     response = requests.get(
         f'http://api.openweathermap.org/data/2.5/weather?q={location}&appid=6d98967004f5e634642db86f5f402d9e')
     if response.status_code == 200:
-        print(response.json())  # Check the response
         return response.json()
 
 
-""" 
-     SAMPLE NEWS API REQUEST
-     request = ''     
-"""
-
 # Get agricultural news function
-url = "https://search-news-feed.p.rapidapi.com/articles"
-
-querystring = {"title": "Agriculture"}
-
-headers = {
-    'x-rapidapi-host': "search-news-feed.p.rapidapi.com",
-    'x-rapidapi-key': "7d443101e6msh648b6cd0a0ddc3fp1a860ejsnd8bfd6be3775"
-}
-
-
 def getAgriNews():
     source_data = []
     articles_url = 'https://newsapi.org/v2/everything?q=farming&apiKey={}'.format(Config.API_KEY)
@@ -50,10 +36,4 @@ def getAgriNews():
     if response.status_code == 200:
         for data in response.json()['articles']:
             source_data.append(data)
-        print(source_data)
         return source_data
-
-
-# Get Trending news function
-def getTrendingNews():
-    pass
